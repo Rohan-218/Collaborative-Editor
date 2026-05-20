9199b995746c812742020081bcac826a217d68bf
Rohan Tomar <159640350+Rohan-218@users.noreply.github.com>
2026-05-20 19:07:13 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Add package.json for project dependencies and scripts
    - Create favicon.svg for the application
    - Add icons.svg containing various SVG icons
    - Include App.css to import Tailwind CSS styles
    - Implement App.jsx as the main application component
    - Set up main.jsx to render the App component
    - Configure Vite with React and Tailwind CSS plugins

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <main
+    className="h-screen w-full bg-gray-950 flex gap-4 p-2"
+  >
+    <aside
+      className="w-1/5 h-full bg-amber-50 rounded-md"
+    ></aside>
+    <section
+      className="w-4/5 h-full bg-neutral-800 rounded-md"
+    ></section>
+
+  </main>
+  )
+}
+
+export default App
